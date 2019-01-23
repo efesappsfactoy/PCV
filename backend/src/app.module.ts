aceacaa9a1e91f34802d7723436e621b8cee9ed8
efren sandoval <efren.sandoval@epn.edu.ec>
2019-01-23 10:35:50 -0500

    Modulo raíz toma los parámetros de conexión para la bd desde archivo externo.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,29 +3,23 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {RestauranteController} from './restaurante/restaurante.controller';
-import { LoginModule } from './login/login.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { PedidosModule } from './pedidos/pedidos.module';
-import { RestauranteModule } from './restaurante/restaurante.module';
+import {LoginModule} from './login/login.module';
+import {UsuarioModule} from './usuario/usuario.module';
+import {PedidosModule} from './pedidos/pedidos.module';
+import {RestauranteModule} from './restaurante/restaurante.module';
+import {DetallePedidoModule} from './detalle-pedido/detalle-pedido.module';
+import {OpcionMenuModule} from './opcion-menu/opcion-menu.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot(
-            {
-                type: 'mysql',
-                host: 'localhost',
-                port: 32773,
-                database: 'PCV',
-                username: 'efren',
-                password: '87654321',
-                synchronize: true, //para nueva bd
-                dropSchema: false,
-                entities: []
-            }),
+        TypeOrmModule.forRoot(),
         LoginModule,
         UsuarioModule,
         PedidosModule,
-        RestauranteModule],
+        RestauranteModule,
+        DetallePedidoModule,
+        OpcionMenuModule
+    ],
     controllers: [AppController, RestauranteController],
     providers: [AppService],
 })
